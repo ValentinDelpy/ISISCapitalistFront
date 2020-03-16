a7c65304cf1fa9fb42352399d5bcabde1d6b757c
Valentin Delpy <soulhokib@gmail.com>
2020-03-16 22:58:44 +0100

    a bit of design

--- FrontSide/src/app/modal/modal.component.ts
+++ FrontSide/src/app/modal/modal.component.ts
@@ -1,28 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 @Component({
   selector: 'app-modal',
-  template: `
- <div (click)="onContainerClicked($event)" class="modal fade" tabindex="-1"
-[ngClass]="{'in': visibleAnimate}"
- [ngStyle]="{'display': visible ? 'block' : 'none', 'opacity':
-visibleAnimate ? 1 : 0}">
- <div class="modal-dialog">
- <div class="modal-content">
- <div class="modal-header">
- <ng-content select=".app-modal-header"></ng-content>
- </div>
- <div class="modal-body">
- <ng-content select=".app-modal-body"></ng-content>
- </div>
- <div class="modal-footer">
- <ng-content select=".app-modal-footer"></ng-content>
- </div>
- </div>
- </div>
- </div>
- `
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
+
   public visible = false;
   public visibleAnimate = false;
   public show(): void {
@@ -34,8 +18,13 @@ export class ModalComponent {
     setTimeout(() => this.visible = false, 300);
   }
   public onContainerClicked(event: MouseEvent): void {
-    if ((event.target as HTMLElement).classList.contains('modal')) {
+    if ((<HTMLElement> event.target).classList.contains('modal')) {
       this.hide();
     }
   }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
 }

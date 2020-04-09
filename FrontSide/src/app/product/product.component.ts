787af1a0e6a3abd35e1e477703905eef55940fb2
Curt Bryan <bryan.curt@univ-jfc.fr>
2020-04-09 03:30:41 +0200

    changement de progress back en snack bar manager fix

--- FrontSide/src/app/product/product.component.ts
+++ FrontSide/src/app/product/product.component.ts
@@ -21,6 +21,7 @@ export class ProductComponent implements OnInit, AfterViewInit {
   isRun: boolean;
   lastupdate: number;
   maxAchat: number;
+  progressbarvalue: number = 0;
 
   // tslint:disable-next-line:variable-name
   _money: number;
@@ -35,8 +36,9 @@ export class ProductComponent implements OnInit, AfterViewInit {
     this.maxAchat = this.product.cout;
     if (this.product.managerUnlocked && this.product.timeleft > 0) {
       this.lastupdate = Date.now();
-      this.progress = (this.product.vitesse - this.product.timeleft) / this.product.vitesse;
-      this.bar.animate(1, { duration: this.progress });
+      //this.progress = (this.product.vitesse - this.product.timeleft) / this.product.vitesse;
+      this.progressbarvalue = this.product.vitesse;
+      //this.bar.animate(1, { duration: this.progress });
     }
   }
 
@@ -66,7 +68,7 @@ export class ProductComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     setTimeout(() => {
-      this.bar = new ProgressBar.Line(this.progressBarItem.nativeElement, {
+      /*this.bar = new ProgressBar.Line(this.progressBarItem.nativeElement, {
         strokeWidth: 4,
         easing: 'easeInOut',
         color: '#ffff00',
@@ -78,27 +80,20 @@ export class ProductComponent implements OnInit, AfterViewInit {
         step: (state, bar) => {
           bar.path.setAttribute('stroke', state.color);
         }
-      });
+      });*/
     }, 100)
 
   }
   startFabrication() {
-    if (this.product.quantite >= 1) {
-      const progress = (this.product.vitesse - this.product.timeleft) / this.product.vitesse;
-      this.bar.animate(1, { duration: this.progress });
-      this.product.timeleft = this.product.vitesse;
-      this.lastupdate = Date.now();
-      this.isRun = true;
-      console.log('test2');
-    }
+    this.product.timeleft = this.product.vitesse;
+    this.lastupdate = Date.now();
   }
 
   calcScore() {
 
     // si le produit n'est pas en production mais que le manager est débloqué, on le lance
-    if (this.product.managerUnlocked) {
+    if (this.product.timeleft === 0 && this.product.managerUnlocked) {
       this.startFabrication();
-      this.product.timeleft = this.product.timeleft - (Date.now() - this.lastupdate);
     }
     // on ne fait rien si le produit n'est pas en production
     else if (this.product.timeleft > 0) {
@@ -111,21 +106,19 @@ export class ProductComponent implements OnInit, AfterViewInit {
       // si le temps de production du produit est écoulé...
       if (this.product.timeleft <= 0) {
         this.product.timeleft = 0;
-        this.lastupdate = 0;
-        this.isRun = false;
-        this.bar.set(0);
-        this.notifyProduction.emit(this.product);
+        this.progressbarvalue = 0;
         // on prévient le composant parent que ce produit a été généré.
         this.notifyProduction.emit(this.product);
         // et on relance si jamais le manager est débloqué
         if (this.product.managerUnlocked) {
-          this.product.timeleft = this.product.timeleft - (Date.now() - this.lastupdate);
-          console.log(this.product.managerUnlocked);
-
+          this.startFabrication();
         }
       }
+      // on calcule le positionnement de la barre de progression en pourcentage
+      else this.progressbarvalue = ((this.product.vitesse - this.product.timeleft) / this.product.vitesse) * 100
     }
   }
+
 
   achatProduct() {
     console.log(this.calcMaxCanBuy())
@@ -138,13 +131,13 @@ export class ProductComponent implements OnInit, AfterViewInit {
       this.notifyMoney.emit(coutAchat);
       this.product.quantite = this.product.quantite + this._qtmulti;
       // bonus d'achat spécifique à chaque produit
-      this.product.palliers.pallier.forEach(value => {
-        if (!value.unlocked && this.product.quantite > value.seuil) {
-          this.product.palliers.pallier[this.product.palliers.pallier.indexOf(value)].unlocked = true;
+      //this.product.palliers.pallier.forEach(value => {
+        //if (!value.unlocked && this.product.quantite > value.seuil) {
+          //this.product.palliers.pallier[this.product.palliers.pallier.indexOf(value)].unlocked = true;
          // this.calcUpgrade(value);
          // this.notifyService.showSuccess("déblocage d'un bonus " + value.typeratio + " effectué pour " + this.product.name, "BONUS")
-        }
-      });
+        //}
+     // });
     }
   }
 
